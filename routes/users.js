4dbb3c534239a8a1b63b046fbd0f8d79e559f92f
Harish Shah <harisshah9867@gmail.com>
2024-04-21 21:34:45 +0530

    initial commit for Backend Code Social-Medail App

--- routes/users.js
+++ routes/users.js
@@ -14,7 +14,7 @@ router.get("/:id", verifyToken, getUser);
 router.get("/:id/friends", verifyToken, getUserFriends);
 
 // Update
-router.patch("/:id/friendId", verifyToken, addRemoveFriend);
+router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
 
 export default router;
 
